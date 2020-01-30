c14584cbbef396194251f19dbcd1ac91a2755d47
Polly <polly.kummerfeldt@synaptech-usa.com>
2020-01-29 20:18:25 -0700

    SSDP-948 Modified font name of the TestIcon package

--- icon-packs/ikonli-feather-pack/src/main/java/module-info.java
+++ icon-packs/ikonli-feather-pack/src/main/java/module-info.java
@@ -18,7 +18,8 @@
 module org.kordamp.ikonli.feather {
     requires org.kordamp.iconli.core;
     requires static org.kordamp.jipsy;
-    exports org.kordamp.ikonli.feather;
+  requires org.kordamp.jipsy.annotations;
+  exports org.kordamp.ikonli.feather;
 
     provides org.kordamp.ikonli.IkonHandler
         with org.kordamp.ikonli.feather.FeatherIkonHandler;
